1976c5370ff040927153a47072b84684d98736cd
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-02-13 19:31:58 +0200

    solve p5

--- app.js
+++ app.js
@@ -104,8 +104,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
   //eslint-disable-line
+  let m=1;
+  for (let i =0 ; i< multArr.length;i++ ){
+    m=multiply(m,multArr[i])[0];    
+  }
+  return[m,`The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${m}.`];
 }
-
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
